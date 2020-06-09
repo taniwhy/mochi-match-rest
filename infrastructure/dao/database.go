1f2f4b0153d806038fe7a4d576e09968da1d3492
taniwhy <tanitani628@gmail.com>
2020-06-09 13:18:05 +0900

    test

--- infrastructure/dao/database.go
+++ infrastructure/dao/database.go
@@ -1,9 +1,17 @@
 package dao
 
 import (
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+
+	//
+	_ "github.com/lib/pq"
+
 	"github.com/jinzhu/gorm"
 	"github.com/taniwhy/mochi-match-rest/config"
 
+	//
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	// postgres driver
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -23,5 +31,12 @@ func NewDatabase() *gorm.DB {
 	if err != nil {
 		panic(err.Error())
 	}
+	driver, err := postgres.WithInstance(conn.DB(), &postgres.Config{})
+	m, _ := migrate.NewWithDatabaseInstance(
+		"file://migrations", // マイグレーションファイルがあるディレクトリの指定
+		"mysql",
+		driver,
+	)
+	m.Steps(2)
 	return conn
 }

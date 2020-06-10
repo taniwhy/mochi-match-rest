18d9072f57e37489f02d9fa88d62fe0a243b8302
taniwhy <>
2020-06-10 10:03:42 +0900

    update config

--- config/config.go
+++ config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"os"
 	"strconv"
 
@@ -18,17 +17,7 @@ func init() {
 }
 
 // GetDatabaseConf :
-func GetDatabaseConf() (dsn string) {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	pass := os.Getenv("DB_PASS")
-	dbname := os.Getenv("DB_NAME")
-	dsn = fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, pass, dbname,
-	)
-	return
-}
+func GetDatabaseConf() (dsn string) { return os.Getenv("DB_URL") }
 
 // GetRedisConf :
 func GetRedisConf() (size int, network, addr, pass, key string) {

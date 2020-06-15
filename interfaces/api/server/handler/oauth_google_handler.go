6c039a563983d81e434f820e400b1a00f7da5537
taniwhy <>
2020-06-15 11:20:30 +0900

    auth_handler修正

--- interfaces/api/server/handler/oauth_google_handler.go
+++ interfaces/api/server/handler/oauth_google_handler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -96,7 +97,7 @@ func (gA *googleOAuthHandler) Callback(c *gin.Context) {
 	}
 	if ok {
 		c.SetCookie("pid", gU.ID, 0, "/", "", false, true)
-		c.SetCookie("name", gU.Name, 100, "/", "localhost", false, true)
+		c.SetCookie("name", gU.Name, int(time.Now().Add(time.Minute*90).Unix()), "/", "localhost", false, true)
 		c.SetCookie("mail", gU.Email, 0, "/", "localhost", false, true)
 		// ユーザー登録ページにリダイレクト
 		c.Redirect(http.StatusTemporaryRedirect, "http://localhost:4000/sign-up")
